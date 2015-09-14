f727668c069c611f8390805684dec9b45d89ba4c
Bruno Scopelliti <bru.scopelliti@gmail.com>
2015-09-14 18:35:43 +0200

    delegation
    missing tests

---
+++ src/utils/simple-delegation.js
@@ -0,0 +1,31 @@
+
+/**
+ * @todo missing coverage
+ * usage:
+ * document.addEventListener("click", delegate(".elem", function(evt) {});
+ */
+ChromeAppManager.define("delegate", [], function() {
+
+  function match_(target, selector, boundElement){
+    if (target === boundElement){
+      return false;
+    }
+    if (target.matches(selector)){
+      return target;
+    }
+    if (target.parentNode){
+      return match_(target.parentNode, selector, boundElement);
+    }
+    return false;
+  }
+
+  return function delegate(selector, fn){
+    return function(evt) {
+      var el;
+      if(el = match_(evt.target, selector, this)){
+        fn.apply(el);
+      }
+    };
+  };
+
+});
