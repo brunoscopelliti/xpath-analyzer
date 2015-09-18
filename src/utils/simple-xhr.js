a1e9c539bb8936762ba5887ce521e95809e55b4f
Bruno Scopelliti <bru.scopelliti@gmail.com>
2015-09-18 18:11:12 +0200

    simple-xhr module
    GET is enough for me!

---
+++ src/utils/simple-xhr.js
@@ -0,0 +1,37 @@
+
+/**
+ * A simple wrapper to handle xhr GET requests
+ * usage: xhr(url).then(function() { .. }).catch(function() { .. })
+ */
+ 
+ChromeAppManager.define("xhr", [], function() {
+
+  "use strict";
+
+  return function xhr(url){
+
+    return new Promise(function(res, rej) {
+
+      var req = new XMLHttpRequest();
+
+      req.onreadystatechange = function(evt) {
+        if (req.readyState==4) {
+          if (req.status==200){
+            return res({ status: req.status, xml: req.responseXML });
+          }
+          return rej(req.status);
+        }
+      };
+
+      req.onerror = function(evt){
+        return rej(evt);
+      };
+
+      req.open('GET', url, true);
+      req.send();
+
+    });
+
+  };
+
+});
